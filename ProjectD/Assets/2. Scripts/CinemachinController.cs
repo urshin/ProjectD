6f25c3d1cb9e8e28ccc26770c3a5af3586b95e23
Urshin <yesjune77@gmail.com>
2024-04-09 19:47:40 +0900

    car camera
    추가
    -camera *2
    삭제
    x
    변경
    -cinemachin camera

---
+++ ProjectD/Assets/2. Scripts/CinemachinController.cs
@@ -0,0 +1,46 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachinController : MonoBehaviour
+{
+    [SerializeField] CinemachineStateDrivenCamera cinemachineStateDrivenCamera;
+    int cameraNum = 1;
+    int MaxCamera;
+    [SerializeField] Animator cinemachineAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            cameraNum++;
+            if(cameraNum > 3)
+            {
+                cameraNum = 1;
+            }
+        }
+
+        if (cameraNum == 1)
+        {
+            cinemachineAnimator.Play("1stView");
+        }
+        if (cameraNum == 2)
+        {
+            cinemachineAnimator.Play("3rdVeiw");
+        }
+        if (cameraNum == 3)
+        {
+            cinemachineAnimator.Play("GroundView");
+        }
+    }
+}
